5f43bf1260d43ffe6be9411062e4f3b07272c7ed
Brandon Davis <brandon.davis@conehealth.com>
2022-07-05 15:32:04 -0400

    adjusted spacing for different screen sizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log(dayjs());
+
+var apiKey = "9c59fa0d777a42a2860191113220507"
+
+var apiUrl = "https://api.weatherapi.com/v1/forecast.json?key="+apiKey+"&q=Rome&days=5"
+
+var getForeCast = function (city) {
+    console.log(apiUrl)
+    var response = fetch(apiUrl);
+
+
+    console.log(response);
+
+
+}
+//web call
+
