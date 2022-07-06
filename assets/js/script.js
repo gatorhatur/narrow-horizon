6679cb67891f25efdf148510fb1db95484cbd23a
Brandon Davis <brandon.davis@conehealth.com>
2022-07-06 10:43:01 -0400

    working with icons and weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 
-var apiKey = "9c59fa0d777a42a2860191113220507"
+var apiKey = "JDLUYSFGP26C38G5C7DQ5UGV5"
 
-var apiUrl = "https://api.weatherapi.com/v1/forecast.json?key="+apiKey+"&q=Rome&days=5"
 
-var getForeCast = function (city) {
+var city = "Greensboro";
+
+//need a css file with Id's that handle the icons
+
+
+var getForeCast = function () {
+    var apiUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=" + city+"&aggregateHours=24&forecastDays=6&contentType=json&iconSet=icons2&key=" + apiKey;
     console.log(apiUrl)
-    var response = fetch(apiUrl);
+    fetch(apiUrl).then(function (response) {
+        console.log(response);
+    });
 
 
-    console.log(response);
+    //console.log(response);
 
 
 }
